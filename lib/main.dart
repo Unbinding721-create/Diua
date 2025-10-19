d3a0666a6c4cdc0d11a7840446f822df48ada234
Unbinding721-create <yotuber721@gmail.com>
2025-10-20 00:02:18 +0800

    Changed main.dart to function with camera permission

--- lib/main.dart
+++ lib/main.dart
@@ -19,10 +19,59 @@ class MyApp extends StatelessWidget {
         //nothing here
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deeppurple)
-      home: Scaffold(
+        useMaterial3: true,
+      ),
+      home: const (
         appBar: AppBar(title: const Text('Hello Flutter')),
         body: const Center(child: Text('Welcome to Flutter on GitHub Web!')),
       ),
     );
+  }
+}
+
+class Homepage extends StatelessWidget {
+  const Homepage{{super.key}};
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  var isPermissionGranted = Platform.isAndroid ? false : true;
+  static const cameraPermission = MethodChannel("camera_permission");
+
+  @override
+
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) {
+      _getCameraPermission();
+    }
+  }
+
+  Future<void> _getCameraPermission() async {
+    try {
+      final bool result = await camera_Permission.invokeMethod(
+        'getCameraPermission',
+      );
+      if (result) {
+        setState(() {
+          isPermissionGranted = true;
+        });
+      } else {
+        debugPrint("Camera permission deniyd")
+      }
+    } on PlatformException catch (e) {
+      debugPrint("Failed to get biometric: '${e.message}'.");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: isPermissionGranted
+          ? SafeArea(child: const CameraView())
+          : const SafeArea(child: Text("Give camera perwison pwease")),
+    ); //Scaffold
   }
 }
