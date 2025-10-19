52f7e8f0d6a60c61659dc5ab68d73fd30cfffdc1
Unbinding721-create <yotuber721@gmail.com>
2025-10-19 23:29:03 +0800

    Change app title to 'Diua' and update theme colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart.io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,10 +10,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Web Demo',
+      title: 'Diua',
+      theme: ThemeData(
+        //nothing here
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deeppurple)
       home: Scaffold(
         appBar: AppBar(title: const Text('Hello Flutter')),
         body: const Center(child: Text('Welcome to Flutter on GitHub Web!')),
