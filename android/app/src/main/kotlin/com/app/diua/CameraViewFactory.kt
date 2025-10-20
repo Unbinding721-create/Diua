ad15ab306d1f4a586153179458ba1420724c2a73
Unbinding721-create <yotuber721@gmail.com>
2025-10-20 18:22:05 +0800

    Made the CameraViewFactory go with the other scripts

--- android/app/src/main/kotlin/com/app/diua/CameraViewFactory.kt
+++ android/app/src/main/kotlin/com/app/diua/CameraViewFactory.kt
@@ -1,15 +1,27 @@
 package com.app.diua
 
 import android.content.Context
+import androidx.lifecycle.LifecycleOwner
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.StandardMessageCodec
 import io.flutter.plugin.platform.PlatformView
 import io.flutter.plugin.platform.PlatformViewFactory
 
-class CameraViewFactory(val activity: FlutterActivity,private val messenger: BinaryMessenger) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+class CameraViewFactory(
+    // activity must be declared as val to be accessible inside create()
+    val activity: FlutterActivity, 
+    private val messenger: BinaryMessenger
+) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+
     override fun create(context: Context, viewId: Int, args: Any?): PlatformView {
         val creationParams = args as Map<String?, Any?>?
-        return CameraView(context,messenger, viewId, creationParams,activity)
+        
+        val cameraView = CameraView(context, messenger, viewId, creationParams, activity)
+        
+        // hi there
+        (activity as LifecycleOwner).lifecycle.addObserver(cameraView)
+        
+        return cameraView
     }
 }
